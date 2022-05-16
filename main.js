ae9f6e63318b7b000935700a51816ea8edc39d8c
Sebastien-Gineste <47150778+Sebastien-Gineste@users.noreply.github.com>
2022-05-16 22:03:58 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,10 @@
+// -------------------------------
+// Fichier généré automatiquement par polyconquest :
+// Veuillez utiliser ce fichier pour envoyer votre code
+// -------------------------------
+//console.log("move 1 5");
+//console.log("move 5 6");
+//console.log("move 6 0");
+console.log("move 1 0");
+console.log("move 0 4");
+console.log("move 4 8");
