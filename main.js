ac169b09ef49d5c047b1bf25a8f7537654b94e0a
Sebastien-Gineste <47150778+Sebastien-Gineste@users.noreply.github.com>
2022-05-17 10:32:10 +0200

    update game

--- main.js
+++ main.js
@@ -2,9 +2,31 @@
 // Fichier généré automatiquement par polyconquest :
 // Veuillez utiliser ce fichier pour envoyer votre code
 // -------------------------------
-//console.log("move 1 5");
-//console.log("move 5 6");
-//console.log("move 6 0");
+
+// Reads the data of the game 
+const content = JSON.parse(readline());
+/* graph = {
+    planets : [
+        {
+            id:number,
+            name: string,
+            position: {x: number, y: number}
+        },...
+    ],
+    paths : [
+        {
+            src: 1, // id of the planet
+            dest: 0, // id of the planet
+            length :  number // path length
+        },...
+    ],
+    startingPoint: number : id of the planet,
+    endPoints : number[] ids of the planets
+}
+*/
+const graph = content.graph; 
+// carburant : number
+const carburant = content.carburant;
+
+// move the ship from the planet 1 to the planet 2
 console.log("move 1 0");
-console.log("move 0 4");
-console.log("move 4 8");
